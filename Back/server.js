61d4f30b41eb8b626c688487d2fa3a00c1cecd33
Breno Vieira <brenovnc435@gmail.com>
2024-06-27 15:20:26 -0300

    Colocando express, cors e nodemon no back e criando um git ignore para os node_moudules

---
+++ Back/server.js
@@ -0,0 +1,15 @@
+//importar libs externas
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send('Hello Word!')
+});
+
+app.listen(3000, ()=> {
+    console.log("Listening on port 3000")
+});
